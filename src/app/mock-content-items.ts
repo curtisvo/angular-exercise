62309bc7f1263097f519be98f4ec39dc65e56519
Curtis van Osch <curtis.van-osch@hec.ca>
2018-11-17 14:42:26 -0500

    commit mock data

---
+++ src/app/mock-content-items.ts
@@ -0,0 +1,10 @@
+export const CONTENT_ITEMS = {
+    1: [{
+        title: 'Game of Thrones',
+	filename: 'got.jpg'
+	},
+	{
+	    title: 'Brooklyn 99',
+	    filename: 'got.jpg'
+	}]
+};
---
+++ src/app/mock-nav-items.ts
@@ -0,0 +1,1 @@
+export const NAV_ITEMS = ['Suggestions for you', 'Recently Watched', 'New Arrivals', 'Instant Queue'];
