9b20c068df9ae7de90a144248bbd9cdd264d6361
Curtis van Osch <curtis.van-osch@hec.ca>
2018-11-17 15:43:53 -0500

    nav items

--- src/app/mock-nav-items.ts
+++ src/app/mock-nav-items.ts
@@ -1,1 +1,17 @@
-export const NAV_ITEMS = ['Suggestions for you', 'Recently Watched', 'New Arrivals', 'Instant Queue'];
+export const NAV_ITEMS = 
+	[{
+		id: 1,
+		title: 'Suggestions for you'
+	},
+	{
+		id: 2, 
+		title: 'Recently Watched'
+	},
+        {
+		id: 3,
+		title: 'New Arrivals' 
+	},
+	{
+		id: 4,
+		title: 'Instant Queue'
+	}];
