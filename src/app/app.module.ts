c515be5cf7257740defc394403402edad32c63d6
Curtis van Osch <curtis.van-osch@hec.ca>
2018-11-17 12:12:45 -0500

    init project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
