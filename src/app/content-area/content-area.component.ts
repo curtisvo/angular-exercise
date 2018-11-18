306ff13e353df59e5fd73d6c82d66e7d1945e5a8
Curtis van Osch <curtis.van-osch@hec.ca>
2018-11-18 11:28:08 -0500

    first try at content keyboard control

--- src/app/content-area/content-area.component.ts
+++ src/app/content-area/content-area.component.ts
@@ -11,14 +11,17 @@ import { CONTENT_ITEMS } from '../mock-content-items';
 export class ContentAreaComponent implements OnInit {
   private _selectedListIndex: number;
   @Input() currentNavTitle;
+  private _focus;
   @Input() focus;
 
   mockContentItems = CONTENT_ITEMS;
   showsList;
+  selectedShowIndex;
 
   constructor() { }
 
   ngOnInit() {
+    this.selectedShowIndex = null;
   }
 
   // react to changes in the selected list index
@@ -27,10 +30,30 @@ export class ContentAreaComponent implements OnInit {
     this._selectedListIndex = index;
     this.showsList = this.mockContentItems[index];
   }
-
+  /*
+  @Input()
+  set focus(focus) {
+    if (this._focus === false && focus === true) {
+      this.selectedShowIndex = 0;
+      }
+      else if (this._focus === true && focus === false) {
+      this.selectedShowIndex = null;
+      }
+      console.log("setfocus");
+    this._focus = focus;
+  }
+  */
   // Adding an event listener to both components will not offer the best performance
   // TODO research other ways
   onKey(event: any) {
+    if(event.key === "ArrowRight" && this.focus) {
+      if (this.selectedShowIndex === null) {
+        this.selectedShowIndex = 0;
+      }
+      else if (this.selectedShowIndex < 3) {
+      	this.selectedShowIndex++;
+      }
+    }
   }
 
 }
